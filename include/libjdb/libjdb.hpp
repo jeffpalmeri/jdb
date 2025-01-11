c8960c547e6f3b592685ec80c93f4aae7ea70298
Jeff <jeffpalmeri184@gmail.com>
2025-01-11 15:30:05 -0600

    chapter 1

---
+++ include/libjdb/libjdb.hpp
@@ -0,0 +1,8 @@
+#ifndef JDB_LIBJDB
+#define JDB_LIBJDB
+
+namespace jdb {
+  void say_hello();
+}
+
+#endif
---
+++ src/libjdb.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <libjdb/libjdb.hpp>
+
+void jdb::say_hello() {
+  std::cout << "Hello jdb vcpkg what the heck\n";
+}
