9b05d29d00fb900d74e40fffa92d698d7acb3c9b
Jeff <jeffpalmeri184@gmail.com>
2025-01-21 18:22:37 -0600

    chapter 3 have not tested yet

--- tools/jdb.cpp
+++ tools/jdb.cpp
@@ -1,70 +1,66 @@
+// #include <cstring>
+#include <string.h>
 #include <iostream>
 #include <unistd.h>
 #include <libjdb/libjdb.hpp>
-
 #include <string_view>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
 #include <string>
 #include <editline/readline.h>
-
-#include <vector>
-
-namespace {
-  pid_t attach(int argc, char** argv) {
-    pid_t pid = 0;
-    if(argc == 3 && argv[1] == std::string_view("-p")) {
-      pid = std::atoi(argv[2]);
-      std::cout << "THE PID IS " << pid << std::endl;
-      if(pid <= 0) {
-        std::cerr << "Invalid pid\n";
-        return -1;
-      }
-      if(ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) < 0) {
-        std::perror("Could not attach");
-        return -1;
-      }
-    } 
-    else {
-      const char * program_path = argv[1];
-      if((pid = fork()) < 0) {
-        std::perror("fork failed");
-        return -1;
-      }
-
-      if(pid == 0) {
-        // child process
-        if(ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
-          std::perror("tracing failed");
-        }
-        if(execlp(program_path, program_path, nullptr) < 0) {
-          std::perror("execlp failed");
-          return -1;
-        }
-      }
-    }
-    return pid;
-  }
-}
-
 #include <algorithm>
 #include <sstream>
+#include <vector>
+#include <libjdb/process.hpp>
+#include <libjdb/error.hpp>
 
 namespace {
+  std::unique_ptr<jdb::process> attach(int argc, const char** argv) {
+    // Passing PID
+    if(argc == 3 && argv[1] == std::string_view("-p")) {
+      pid_t pid = std::atoi(argv[2]);
+      return jdb::process::attach(pid);
+    }
+
+    // Passing program name
+    else {
+      const char *program_path = argv[1];
+      return jdb::process::launch(program_path);
+    }
+  }
+
+  void print_stop_reason(const jdb::process& process, jdb::stop_reason reason) {
+    std::cout << "Process " << process.pid() << ' ';
+
+    switch(reason.reason) {
+      case jdb::process_state::exited:
+        std::cout << "exited with status " << static_cast<int>(reason.info);
+        break;
+      case jdb::process_state::terminated:
+        std::cout << "terminated with signal " << sigabbrev_np(reason.info);
+        break;
+      case jdb::process_state::stopped:
+        std::cout << "stopped with signal " << sigabbrev_np(reason.info);
+        break;
+      case jdb::process_state::running:
+        std::cout << "running state is by definition not stopped, this should never happen";
+        break;
+    }
+    std::cout << std::endl;
+  }
+
   std::vector<std::string> split(std::string_view str, char delimiter);
   bool is_prefix(std::string_view str, std::string_view of);
-  void resume(pid_t pid);
-  void wait_on_signal(pid_t pid);
 
-  void handle_command(pid_t pid, std::string_view line) {
+  void handle_command(std::unique_ptr<jdb::process>& process, std::string_view line) {
     auto args = split(line, ' ');
     auto command = args[0];
 
     if(is_prefix(command, "continue")) {
-      resume(pid);
-      wait_on_signal(pid);
+      process->resume();
+      auto reason = process->wait_on_signal();
+      print_stop_reason(*process, reason);
     }
     else {
       std::cerr << "Unknown command\n";
@@ -87,55 +83,46 @@ namespace {
     return std::equal(str.begin(), str.end(), of.begin());
   }
 
-  void resume(pid_t pid) {
-    if(ptrace(PTRACE_CONT, pid, nullptr, nullptr) < 0) {
-      std::cerr << "Couldn't continue\n";
-      std::exit(-1);
-    }
-  }
+  void main_loop(std::unique_ptr<jdb::process>& process) {
+    char* line = nullptr;
+    while((line = readline("jdb> ")) != nullptr) {
+      std::string line_str;
 
-  void wait_on_signal(pid_t pid) {
-    int wait_status;
-    int options = 0;
-    if(waitpid(pid, &wait_status, options) < 0) {
-      std::perror("waitpid failed");
-      std::exit(-1);
+      if(line == std::string_view("")) {
+        free(line);
+        if(history_length > 0) {
+          line_str = history_list()[history_length - 1]->line;
+        }
+        else {
+          line_str = line;
+          add_history(line);
+          free(line);
+        }
+
+        if(!line_str.empty()) {
+          try {
+            handle_command(process, line_str);
+          }
+          catch(const jdb::error& err) {
+            std::cout << err.what() << '\n';
+          }
+        }
+      }
     }
   }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, const char** argv) {
   if(argc == 1) {
     std::cerr << "No arguments given\n";
     return -1;
   }
 
-  pid_t pid = attach(argc, argv);
-
-  int wait_status;
-  int options = 0;
-  if(waitpid(pid, &wait_status, options) < 0) {
-    std::perror("waitpid failed");
+  try {
+    auto process = attach(argc, argv);
+    main_loop(process);
   }
-
-  char *line = nullptr;
-  while((line = readline("jdb> ")) != nullptr) {
-    std::string line_str;
-
-    if(line == std::string_view("")) {
-      free(line);
-      if(history_length > 0) {
-        line_str = history_list()[history_length - 1]->line;
-      }
-    }
-    else {
-      line_str = line;
-      add_history(line);
-      free(line);
-    }
-
-    if(!line_str.empty()) {
-      handle_command(pid, line_str);
-    }
+  catch (const jdb::error& err) {
+    std::cout << err.what() << '\n';
   }
 }
